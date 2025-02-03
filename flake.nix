8c2888e11755a73ce2c8bd7cfbd76215a536222e
Jez Cope <j.cope@erambler.co.uk>
2025-02-03 14:30:03 +0000

    Switch to jupyter-book

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          python = pkgs.python312;
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            nativeBuildInputs = [
+              (python.withPackages (py: [
+                py.jupyter
+                py.jupyter-book
+                py.cookiecutter
+              ]))
+            ];
+          };
+        };
+      flake = { };
+    };
+}
--- src/SUMMARY.md
+++
@@ -1,7 +0,0 @@
-# Summary
-
-- [Overview](./overview.md)
-- [Introduction](./intro.md)
-- [Using Python in OpenRefine](./using_python.md)
-- [Translating GREL to Python](./grel_to_python.md)
-- [Next steps](./next_steps.md)
